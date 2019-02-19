7b2c4d0fb0b5f9652215ad4cd2977c0d0cf1d502
adamoping <1261662192@qq.com>
2019-02-19 09:48:56 +0800

    todolist

---
+++ src/TodoList.vue
@@ -0,0 +1,44 @@
+ <template>
+ <div>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="handelSubmit">提交</button>
+  </div>
+  <ul>
+    <todo-item  v-for="(item,index) of list" 
+    :content="item" 
+    :index="index" 
+    :key="index"
+    @delete="handelDelete"
+    >item</todo-item>
+  </ul>
+</div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem"
+export default {
+  components:{
+    'todo-item':TodoItem  
+  },
+  data:function(){
+     return{
+      inputValue:'',
+      list:[]
+     }
+  },
+  methods:{
+    handelSubmit:function(){
+     this.list.push(this.inputValue) ;
+     this.inputValue="";
+    },
+    handelDelete:function(index){
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <li @click="handelDelete">{{content}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+    handelDelete:function(){
+      this.$emit('delete',this.index);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
